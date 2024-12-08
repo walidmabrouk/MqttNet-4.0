db1589b3dd2682bc86cf4729ef69f4ef642e2de2
Walid MABROUK <walid.mabrouk@edu.isetcom.tn>
2024-12-08 13:07:42 +0100

    Creation || Server & Client MqttNet4.0

---
+++ MQTTNetClientApp/MQTTNetClientApp/Program.cs
@@ -0,0 +1,102 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MQTTnet;
+using MQTTnet.Client;
+
+// Create TCP based options using the builder.
+
+var factory = new MqttFactory();
+var _mqttClient = factory.CreateMqttClient();
+Console.Write("MQTT Client ID :");
+string? mqttID = Console.ReadLine();
+// Create client options object
+MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
+                                        .WithClientId(mqttID)
+                                        .WithTcpServer("127.0.0.1");
+var mqttClientOptions = new MqttClientOptionsBuilder()
+            .WithTcpServer("localhost", 5004) // Adresse et port du serveur MQTT
+            .WithClientId(mqttID)        // Identifiant unique du client
+            .WithCleanSession()               // Nettoie les sessions persistantes
+            .Build();
+
+
+
+// Set up handlers
+_mqttClient.ConnectedAsync += _mqttClient_ConnectedAsync;
+
+
+_mqttClient.DisconnectedAsync += _mqttClient_DisconnectedAsync;
+// Connect to the broker
+var connectResult = await _mqttClient.ConnectAsync(mqttClientOptions);
+Console.WriteLine("client connected");
+
+Task _mqttClient_ConnectedAsync(MqttClientConnectedEventArgs arg)
+{
+    Console.WriteLine("Connected x");
+    return Task.CompletedTask;
+};
+Task _mqttClient_DisconnectedAsync(MqttClientDisconnectedEventArgs arg)
+{
+    Console.WriteLine("Disconnected");
+    return Task.CompletedTask;
+};
+Console.WriteLine("");
+Console.Write("Sender or Receiver : ");
+string? choice = Console.ReadLine()?.ToLower();
+
+switch (choice)
+{
+    case "sender":
+       
+        publish("topic/temperature", true);
+        break;
+    case "receiver":
+        Subsribe("topic/temperature");
+        break;
+    default: Console.WriteLine("bad choise :( "); break;
+}
+
+
+
+
+
+async void Subsribe(string topic)
+{
+    var mqttSubscribeOptions = factory.CreateSubscribeOptionsBuilder().WithTopicFilter(topic).Build();
+
+    await _mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+    Console.WriteLine($"MQTT client subscribed to {topic}.");
+    _mqttClient.ApplicationMessageReceivedAsync += async e =>
+    {
+        string payload = e.ApplicationMessage.ConvertPayloadToString();
+        double temperature = Convert.ToDouble(payload); 
+        if (temperature > 37.5)
+            Console.ForegroundColor= ConsoleColor.Red;
+        else
+            Console.ForegroundColor= ConsoleColor.Green;
+        Console.WriteLine($"{payload}");
+    };
+}
+async void publish(string topic,  bool loop)
+{
+    Console.WriteLine($"MQTT client Push Msg to {topic}.");
+    Random random = new Random();
+    do
+    {
+        
+        string msg = (random.NextDouble() * (40.0 - 30.0) + 30.0).ToString("F2");
+        var applicationMessage = new MqttApplicationMessageBuilder()
+                    .WithTopic(topic)
+                    .WithPayload(msg)
+                    .Build();
+        await _mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+        await Task.Delay(3000);
+    } while (loop);
+
+
+}
+
+Thread.Sleep(2000);
+
+Console.WriteLine("Press enter to exit.");
+Console.ReadLine();
+
---
+++ MQTTServer/MQTTServer/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Configurer les options du serveur MQTT
+using MQTTnet;
+using MQTTnet.Server;
+using System.Net;
+using System.Text;
+
+
+
+// Create the options for MQTT Broker
+var options = new MqttServerOptionsBuilder()
+    //Set endpoint to localhost
+    //.WithDefaultEndpointBoundIPAddress(IPAddress.Parse("192.168.1.14"))
+    .WithDefaultEndpoint()
+    // Port is going to use 5004
+    .WithDefaultEndpointPort(5004);
+
+// Create a new mqtt server
+var server = new MqttFactory().CreateMqttServer(options.Build());
+//Add Interceptor for logging incoming messages
+server.InterceptingPublishAsync += Server_InterceptingPublishAsync;
+
+
+server.ClientConnectedAsync += e =>
+{
+    Console.WriteLine($"Client connecté : {e.ClientId}");
+    return Task.CompletedTask;
+};
+
+server.ClientDisconnectedAsync += e =>
+{
+    Console.WriteLine($"Client déconnecté : {e.ClientId}");
+    return Task.CompletedTask;
+};
+try
+{
+    // Démarrage du serveur MQTT
+    await server.StartAsync();
+    Console.WriteLine($"Serveur MQTT démarré sur localhost:{5004}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erreur lors du démarrage du serveur : {ex.Message}");
+}
+
+
+// Attente pour voir si le serveur reste actif
+Console.WriteLine("Appuyez sur Enter pour arrêter le serveur.");
+Console.ReadLine();
+
+
+
+Task Server_InterceptingPublishAsync(InterceptingPublishEventArgs arg)
+{
+    // Convert Payload to string
+    var payload = arg.ApplicationMessage?.Payload == null ? null : Encoding.UTF8.GetString(arg.ApplicationMessage?.Payload);
+
+
+    Console.WriteLine(
+        $" TimeStamp: {DateTime.Now} -- Message: ClientId = {arg.ClientId}, Topic = {arg.ApplicationMessage?.Topic}," +
+        $" Payload = {payload}"
+        );
+    return Task.CompletedTask;
+}
